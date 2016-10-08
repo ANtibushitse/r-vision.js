96ed1771f20d576009068411c6ccc5dd98069426
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-08 18:26:54 +0200

    je vais un peut plus loin dans le scope

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 // fichier app
 // IIFE  imediately invoked fonction expression 
-(function () {
+(function main () {
 	console.log("Welcome! ");
+
+
 	console.log("batiment", batiment); // undifined 
- 	var   batiment = "immeuble neuf";
- 	batiment = " immeuble neuf"; 
- 	console.log("batiment", batiment); // affice immeuble neuf 
+ 		var   batiment = "immeuble neuf";
+ 		batiment = " immeuble neuf"; 
+ 		console.log("batiment", batiment); // affice immeuble neuf 
 
  	function rouler (){
- 		console.log("fct rouler"); 
+ 		console.log("fct rouler");
+ 	
+
+ 		var voiture = "vroom vroom";
+ 		console.log("voiture", voiture) 
+ 		 function bouger (){
+ 		 	console.log("fct bouger"); 
+
+ 		 }
+bouger(); 
+// le scope 
+/* 
+bouger, rouler  
+batiment, rouler 
+
+*/ 
  	}
- 	  rouler(); // appelle de la fonction hors du scope 
+ rouler(); 
+ // le scope contient batiment et rouger 
+
+ 
+
 })();
