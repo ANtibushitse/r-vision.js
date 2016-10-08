409c8e92fc5a6dc4519bfc4a68e16261f868fcc7
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-08 12:17:16 +0200

    r	rtie 1 le scope:wq

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 // IIFE  imediately invoked fonction expression 
 (function () {
 	console.log("Welcome! ");
+	console.log("batiment", batiment); // undifined 
+ 	var   batiment = "immeuble neuf";
+ 	batiment = " immeuble neuf"; 
+ 	console.log("batiment", batiment); // affice immeuble neuf 
+
+ 	function rouler (){
+ 		console.log("ftx rouler"); 
+ 	}
+ 	  
 })();
