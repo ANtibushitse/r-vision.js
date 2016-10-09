484f11e827b9030c2535981f38b2e4e1ccc15550
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-09 21:36:07 +0200

    Le principe du scope selon e prisme de la musique

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 	console.log("Welcome! ");
 
 
-	console.log("musique", dancer); // undifined 
+	console.log("musique", musique); // undifined 
 	var   musique = "suite de tonalité";
-	musique = " immeuble neuf"; 
- 		console.log("batiment", dancer); // affice immeuble neuf 
+	musique = " ensemble de notes créant de la musique"; 
+ 		console.log("musique", musique); // affice immeuble neuf 
 
  		function rouler (){
- 			console.log("fct rouler");
+ 			
 
 
  			var musiqueElectronique = "Boooom Boooom";
  			console.log("musiqueElectronique", musique) 
  			function bouger (){
- 				console.log("fct bouger"); 
+ 				; 
 
  			}
  			bouger(); 
